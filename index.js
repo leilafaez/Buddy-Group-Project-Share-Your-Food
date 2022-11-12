e1a10171584bb670b80b05da9b6b54146a3f190a
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-11-12 21:52:33 +0000

    add records array, add create records route

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
 
+const records = [];
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/api/records", (req, res) => {
+  records.push(req.body);
+
+  console.log(records);
+
+  res.json({ status: "success" });
+});
+
 app.listen(3001, () => { console.log("Listening on port 3001..."); });
